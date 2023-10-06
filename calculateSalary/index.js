8e4a5bc3d93f3aa77774fa21a3450674c64cb771
Adrian <adrian.mokua@icloud.com>
2023-10-06 12:53:54 +0300

    Completed assignment

--- calculateSalary/index.js
+++ calculateSalary/index.js
@@ -1,9 +1,3 @@
-// this is used to input and output salary
-const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
 
